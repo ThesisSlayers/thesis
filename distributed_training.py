c708ec47d6714e7415e7a62a661226ab786ae0e3
ThesisSlayers <ravenmarinelli@gmail.com>
2021-01-21 18:05:57 +0100

    changes in UCF101 file

--- distributed_training.py
+++ distributed_training.py
@@ -14,7 +14,7 @@ import pandas as pd
 from pathlib import Path
 import time
 import warnings
-from datetime import datetime
+from datetime import date
 
 # Cell
 from modules_th.video_block import *
@@ -29,20 +29,20 @@ from modules_th.ucf101 import *
 
 # Cell
 @call_parse
-def main(gpu    :Param("GPU to run on", int)=None,
-         n_lbl  :Param("# of different labels per batch", int)=2,
-         n_el   :Param("# of elements per label", int)=2,
-         l      :Param("Target number of frames of the ResizeTime transform", int)=30,
-         skip   :Param("skip frames",int)=4,
-         size     :Param("size for Resize", int)=224,
-         n_epoch:Param("# of epochs to train", int)=15,
-         loss   :Param("Choose a loss between CEL-SCL and SCL ", str)='SCL',
-         embs_size:Param("embeddings size", int)=256,
-         n_views:Param("number of views", int)=2,
-         log_nm :Param("logs file name. if 'date' the name will be composed by the date", str)='date',
-         descr  :Param("description of the experiment", str)='First training with CE to have a preformance baseline',
-         model: Param("model")='r2p1d50_K',
-         normalize: Param('normalization')='kinetics'
+def main(gpu          : Param("GPU to run on", int)            = None,
+         n_epoch: Param("# of epochs to train", int)           = 0,
+         freeze_epochs : Param("Frozen epochs", int)           = 15,
+         n_lbl  :Param("# of different labels per batch", int) = 2 ,
+         n_el   :Param("# of elements per label", int)         = 2,
+         l      :Param("num of frames of the ResizeTime", int) = 30,
+         skip   :Param("skip frames",int)                      = 2,
+         size     :Param("size for Resize", int)               = 224,
+         loss   :Param(" loss between CEL-SCL and SCL ", str)  = 'SCL',
+         embs_size:Param("embeddings size", int)               = 256,
+         n_views:Param("number of views", int)                 = 2,
+         descr  :Param("description of the experiment", str)   = 'SCL  with 0 unfrozen and 15 frozen epoch',
+         model: Param("model", str)                            = 'r2p1d50_K',
+         normalize: Param('normalization',str)                 = 'kinetics'
         ):
     prefix = '/mnt/data/eugeniomarinelli/'
 
@@ -56,17 +56,31 @@ def main(gpu    :Param("GPU to run on", int)=None,
     learn = get_learner(items, model,  loss, l, size, n_lbl, n_el, skip, embs_size,n_views, normalize)
     print('Learner Loaded \n')
     
+    
+    
     torch.cuda.empty_cache()
     if gpu is not None:
         print("Distributed Data Parallel training started")
         with learn.distrib_ctx(gpu):
             print('starting distrib train \n')
-            learn.fine_tune(n_epoch)
+            learn.fine_tune(n_epoch, freeze_epochs=freeze_epochs) 
     else:
         warnings.filterwarnings("ignore", message='.*nonzero.*', category=UserWarning)
         print("Data Parallel training started")
         with learn.parallel_ctx(device_ids=[0,1]):
             print('starting parallel train \n')
-            learn.fine_tune(n_epoch)
+            learn.fine_tune(n_epoch, freeze_epochs=freeze_epochs)
 
-    save_learner(learn)
+    save_learner(learn, model+'15fr')
+
+    time = date.today().strftime("%d-%m-%y")
+    experiment = pd.DataFrame({'date':[time],
+             'description':[descr],
+             'l':[l],
+             'skip':[skip],
+             'size':[size],
+             'loss':[loss],
+             'n_el':[n_el],
+             'n_lbl':[n_lbl],
+             'n_epochs':[n_epoch],
+             'freeze_epochs':[freeze_epochs]}).to_csv(prefix+'UCF_experiments/experiments_logs.csv',mode='a', header=False)
---
+++ modules_th/None.py
@@ -0,0 +1,27 @@
+
+
+# Cell
+import torch
+import torch.nn as nn
+from fastai.vision.all import *
+from fastai.data.all import *
+from fastai.distributed import *
+import pandas as pd
+from pathlib import Path
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.parameter import Parameter
+from torchvision.io import read_video, VideoReader
+
+# Cell
+import torch
+import torch.nn as nn
+from fastai.vision.all import *
+from fastai.data.all import *
+from fastai.distributed import *
+import pandas as pd
+from pathlib import Path
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.parameter import Parameter
+from torchvision.io import read_video, VideoReader
